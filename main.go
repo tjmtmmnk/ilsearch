cbb081989f8d48ee350cecfbd39dabcbb699c8fb
tjmtmmnk <tsujimoto027@gmail.com>
2020-10-15 04:59:53 +0900

    add page

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
+	"github.com/tjmtmmnk/ilsearch/pkg/history"
 )
 
 var (
@@ -15,9 +15,19 @@ var (
 	searchBar     *tview.InputField
 	preview       *tview.TextView
 	searchResults []SearchResult
+	option        *Option
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	err := history.Init()
+	err = history.WriteHistory(history.Query{
+		Key: "FocusDirectories",
+		Val: "/hoge",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	context = NewContext()
 	s, e := tcell.NewScreen()
 	if e != nil {
@@ -28,7 +38,6 @@ func main() {
 	}
 	app = tview.NewApplication().SetScreen(s)
 	list = tview.NewList().ShowSecondaryText(false)
-	//pages := tview.NewPages()
 
 	searchBar = tview.NewInputField().
 		SetLabel(">>> ").
@@ -39,16 +48,16 @@ func main() {
 		SetDynamicColors(true).
 		SetScrollable(true)
 
-	option := &Option{
+	option = &Option{
 		SearchMode: WordMatch,
 		Directory:  "./",
+		Command:    GitGrep,
 	}
 
 	searchBar.SetChangedFunc(func(text string) {
 		var err error
 		searchResults, err = Search(text, option)
 		if err != nil {
-			fmt.Println(err)
 			log.Fatal("search error")
 		}
 		var resultTexts []string
@@ -74,6 +83,7 @@ func main() {
 		}
 
 		text, err := GetPreviewContent(s, item.fileName, item.lineNum, "OneHalfDark")
+
 		text = tview.TranslateANSI(text)
 		if err != nil {
 			panic(err)
@@ -92,12 +102,26 @@ func main() {
 	list.SetDoneFunc(func() {
 		app.SetFocus(searchBar)
 	})
+	checkbox := searchOptionCheckbox()
 
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(searchBar, 0, 1, true).
+		AddItem(searchBar, 0, 3, true).SetDirection(tview.FlexRow).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
 			AddItem(list, 0, 1, false).
-			AddItem(preview, 0, 1, false), 0, 10, false)
+			AddItem(preview, 0, 1, false), 0, 30, false)
+
+	tree := InitTree(context.workDir)
+
+	searchOption := tview.NewFlex().SetDirection(tview.FlexRow)
+	for _, item := range checkbox.items {
+		searchOption = searchOption.AddItem(item.c, 2, 0, false)
+	}
+
+	pages := tview.NewPages().
+		AddPage("app", flex, true, true).
+		AddPage("tree", tree, true, false).
+		AddPage("option", searchOption, true, false)
+
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch key := event.Key(); {
 		case key == tcell.KeyCtrlS:
@@ -106,11 +130,13 @@ func main() {
 			app.SetFocus(searchBar)
 		case key == tcell.KeyDown && searchBar.HasFocus():
 			app.SetFocus(list)
+		case key == tcell.KeyCtrlF:
+			pages.SwitchToPage("option")
 		}
 		return event
 	})
 
-	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
